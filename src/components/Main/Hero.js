233021ba2fb00c27e49443df24cadc13714a2c8c
llennairek <llennairek@gmail.com>
2021-05-05 23:57:27 +0200

    added some responsiveness on home page

--- src/components/Main/Hero.js
+++ src/components/Main/Hero.js
@@ -4,18 +4,24 @@ import "./Hero.css";
 
 function Hero() {
   return (
-    <div className="hero-container">
-      <div className="hero-overlay"></div>
-      <div className="container hero-card-wrapper">
-        <div className="hero-card">
-          <p>Prêts à faire du tri dans vos placards?</p>
-          <Button
-            text="Commencer à vendre"
-            className="button-green button-big"
-          />
+    <>
+      <div className="hero-container">
+        <div className="hero-overlay"></div>
+        <div className="container hero-card-wrapper">
+          <div className="hero-card">
+            <p>Prêts à faire du tri dans vos placards?</p>
+            <Button
+              text="Commencer à vendre"
+              className="button-green button-big"
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="hero-card-phones">
+        <p>Prêts à faire du tri dans vos placards?</p>
+        <Button text="Commencer à vendre" className="button-green button-big" />
+      </div>
+    </>
   );
 }
 
