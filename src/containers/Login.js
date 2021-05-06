2f6b0e19838b97d472bf6ca154bee5fc022cb196
llennairek <llennairek@gmail.com>
2021-05-06 15:48:16 +0200

    added css for login and signup page

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
+import "./Login.css";
 
 function Login({ handleToken }) {
   const [email, setEmail] = useState("");
@@ -25,26 +26,34 @@ function Login({ handleToken }) {
     }
   };
   return (
-    <div className="login-container">
-      <h2>Se connecter</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          name="login-text-email"
-          id="login-text-email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          name="login-text-password"
-          id="login-text-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input type="submit" value="Se connecter" className="button-green" />
-      </form>
-      <Link to="/signup">Pas encore de compte ? Inscris-toi !</Link>
+    <div className="login-wrapper">
+      <div className="login-container">
+        <h2>Se connecter</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="login-text-email"
+            id="login-text-email"
+            placeholder="Adresse email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="login-text-password"
+            id="login-text-password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="submit"
+            value="Se connecter"
+            className="button-green button-big"
+          />
+        </form>
+        <Link to="/signup">Pas encore de compte ? Inscris-toi !</Link>
+      </div>
     </div>
   );
 }
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
+import "./Signup.css";
 
 function Signup({ handleToken }) {
   const [username, setUsername] = useState("");
@@ -27,33 +28,42 @@ function Signup({ handleToken }) {
     }
   };
   return (
-    <div className="signup-container">
-      <h2>S'inscrire</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="signup-text-input"
-          id="signup-text-input"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="email"
-          name="signup-text-email"
-          id="signup-text-email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          name="signup-text-password"
-          id="signup-text-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input type="submit" value="S'inscrire" className="button-green" />
-      </form>
-      <Link to="/login">Tu as déjà un compte ? Connecte-toi !</Link>
+    <div className="signup-wrapper">
+      <div className="signup-container">
+        <h2>S'inscrire</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="signup-text-input"
+            id="signup-text-input"
+            placeholder="Nom d'utilisateur"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="email"
+            name="signup-text-email"
+            id="signup-text-email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="signup-text-password"
+            id="signup-text-password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="submit"
+            value="S'inscrire"
+            className="button-green button-big"
+          />
+        </form>
+        <Link to="/login">Tu as déjà un compte ? Connecte-toi !</Link>
+      </div>
     </div>
   );
 }
